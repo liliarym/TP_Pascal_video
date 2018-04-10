087031db0e98c057e6fedc2623359cec8bb2bddf
Rym Ghezali <ghezalirym@gmail.com>
2018-04-10 17:16:22 -0400

    version_3

--- app/src/main/java/com/example/eleves/excercie_video/HomeActivity.java
+++ app/src/main/java/com/example/eleves/excercie_video/HomeActivity.java
@@ -3,6 +3,9 @@ package com.example.eleves.excercie_video;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,5 +16,15 @@ public class HomeActivity extends AppCompatActivity {
 
         GridView gridView = (GridView) findViewById(R.id.MyGridView);
         gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(),FullImageActivity.class);
+                i.putExtra("id", position);
+                startActivity(i);
+
+            }
+        });
     }
 }
